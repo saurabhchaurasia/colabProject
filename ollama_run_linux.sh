cafa345936441552ae28d1a5ba15047924779f27
sa <sa>
2026-07-19 17:31:27 +0530

    updating link

--- ollama_run_linux.sh
+++ ollama_run_linux.sh
@@ -3,17 +3,17 @@ set -euo pipefail
 
 # unsloth/Qwen3.5-0.8B-MTP-GGUF:Q4_K_M
 # unsloth/Qwen3.5-27B-MTP-GGUF:Q5_K_M
-MODEL="${MODEL:-hf.co/unsloth/Qwen3.5-0.8B-MTP-GGUF:Q4_K_M}"
+MODEL="${MODEL:-unsloth/Qwen3.5-0.8B-MTP-GGUF:Q4_K_M}"
 CONTEXT_SIZE="${CONTEXT_SIZE:-32000}"
 
 export OLLAMA_CONTEXT_LENGTH="$CONTEXT_SIZE"
 export OLLAMA_KEEP_ALIVE="-1"
 
-ollama pull "$MODEL"
+ollama pull "hf.co/${MODEL}"
 echo "Model '${MODEL}' pulled successfully."
 
 echo "Testing model..."
-ollama run "$MODEL" "Hi"
+ollama run "hf.co/${MODEL}" "Hi"
 
 echo "================================"
 echo "ollama ps"
